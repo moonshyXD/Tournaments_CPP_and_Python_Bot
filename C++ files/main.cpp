041f4548bd579cacd7e963508a167ccea70a2a5a
moonshyXD <163870570+moonshyXD@users.noreply.github.com>
2024-08-01 00:30:08 +0400

    Update main.cpp

--- C++ files/main.cpp
+++ C++ files/main.cpp
@@ -4,9 +4,6 @@ int main(int argc, char *argv[]) {
     try {
         GeneralMessages::printMessage("COMMAND_EXECUTING");
 
-//        int argc = 9;
-//        char* argv[] = {"","","","","","","","C:\\Users\\nadez\\Desktop\\tests\\test.txt","loadParticipantsFromFile"};
-
         Operations operation{};
         operation.handleEvents(argc, argv);
 
@@ -18,5 +15,3 @@ int main(int argc, char *argv[]) {
 }
 
 // Массив disciplines ResultManager.cpp -> checkFirst();
-
-// логирование (в питоне есть, в плюсах нет)
